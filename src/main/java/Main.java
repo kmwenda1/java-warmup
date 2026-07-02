aea60382ad25e29c5cefd4c86f0979e56109c91c
Brianmwe <mwenda.21472@students.kyu.ac.ke>
2026-07-02 18:21:13 +0300

    Add for and while loop examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String day = "Monday";
-        switch (day) {
-            case "Monday":
-                System.out.println("Hunting Day");
-                break;
-            case "Tuesday":
-                System.out.println("Fishing Day");
-                break;
-            case "Wednesday":
-                System.out.println("Hiking Day");
-                break;
-            case "Thursday":
-                System.out.println("Swimming Day");
-                break;
-            case "Friday":
-                System.out.println("Rest Day");
-                break;
-            default:
-                System.out.println("Invalid day");
-                break;
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
         }
     }
 }
