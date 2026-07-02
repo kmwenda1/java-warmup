c85cb979db32b7021c0f63e93b6e968e666bc5fc
Brianmwe <mwenda.21472@students.kyu.ac.ke>
2026-07-02 18:38:49 +0300

    Add methods example - getGrade

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i);
-            i++;
+        char grade = getGrade(85);
+        System.out.println(grade);
+    }
+    static char getGrade(int score) {
+        if (score >= 90) {
+            return 'A';
+        } else if (score >= 80) {
+            return 'B';
+        } else if (score >= 70) {
+            return 'C';
+        } else if (score >= 60) {
+            return 'D';
+        } else {
+            return 'F';
         }
     }
 }
